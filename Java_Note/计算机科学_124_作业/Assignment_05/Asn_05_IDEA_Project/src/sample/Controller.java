50456e5b94a197626ad2a320dbcf496155d0f3ed
Xinghao Zuo <55333469+LinusAuditore@users.noreply.github.com>
2020-04-02 17:17:23 +0800

    Add asn 05

--- Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
+++ Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+    @FXML
+    private ScrollPane LayoutPane;
+    @FXML
+    private TextArea LineItemInfo;
 }
--- Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Main.java
+++ Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,6 +19,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
