30dddbf6a22f4858bb3a680cd014c0c027b026ed
Xinghao Zuo <55333469+LinusAuditore@users.noreply.github.com>
2020-04-02 16:35:33 +0800

    Add asn 05

--- Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Main.java
+++ Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("GUI Pizza Bestellsystem");
+        primaryStage.setTitle("GUI Pizza Order System Ver. 114514");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
