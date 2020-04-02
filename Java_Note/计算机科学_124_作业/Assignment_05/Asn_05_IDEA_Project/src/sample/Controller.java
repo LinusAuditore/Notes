b7ae695ca4be0781f7f0d2a664479784d55342d5
Xinghao Zuo <55333469+LinusAuditore@users.noreply.github.com>
2020-04-02 17:35:56 +0800

    Add asn 05

--- Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
+++ Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
@@ -1,13 +1,28 @@
 package sample;
 
-
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 
 public class Controller {
     @FXML
     private ScrollPane LayoutPane;
     @FXML
     private TextArea LineItemInfo;
+    @FXML
+    private Button LineItemGenerator;
+    @FXML
+    private TextField Number;
+    @FXML
+    private ChoiceBox Ham;
+    @FXML
+    private ChoiceBox Pepper;
+    @FXML
+    private ChoiceBox Pine;
+    @FXML
+    private ChoiceBox Cheese;
+    @FXML
+    private ChoiceBox Vege;
+    @FXML
+    private ChoiceBox Size;
+    private LineItem theLine;
 }
