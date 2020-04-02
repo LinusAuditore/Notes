23421a5807824f4898564ca708eb60060873d46b
Xinghao Zuo <55333469+LinusAuditore@users.noreply.github.com>
2020-04-02 19:37:07 +0800

    Add asn 05

--- Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
+++ Java_Note/计算机科学_124_作业/Assignment_05/Asn_05_IDEA_Project/src/sample/Controller.java
@@ -1,7 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import javax.swing.*;
 
 public class Controller {
     @FXML
@@ -24,7 +27,37 @@ public class Controller {
     private ChoiceBox Vegetarian;
     @FXML
     private ChoiceBox Size;
+    @FXML
+    private TextField UnitPrice;
+    @FXML
+    private Button DisplayUnitPrice;
     private LineItem theLine;
     private Pizza pizza;
+
+    final String[] ham = new String[]{"Single", "None"};
+    final String[] pepper = new String[]{"Single", "None"};
+    final String[] pine = new String[]{"Single", "None"};
+    final String[] cheese = new String[]{"Single", "Double", "Triple"};
+    final String[] size = new String[]{"Small", "Medium", "Large"};
+    final Boolean[] vegetarian = new Boolean[]{true, false};
+
+    // Events Start here
+    // Create a Pizza and display the unit price
+    @FXML
+    private void SinglePizza (ActionEvent event){
+        String HamChoice = ham[Ham.getSelectionModel().getSelectedIndex()];
+        String PepperChoice = pepper[Pepper.getSelectionModel().getSelectedIndex()];
+        String PineChoice = pine[Pine.getSelectionModel().getSelectedIndex()];
+        String CheeseChoice = cheese[Cheese.getSelectionModel().getSelectedIndex()];
+        String SizeChoice = size[Size.getSelectionModel().getSelectedIndex()];
+        Boolean VegetarianChoice = vegetarian[Vegetarian.getSelectionModel().getSelectedIndex()];
+        try{
+            this.pizza = new Pizza(SizeChoice, VegetarianChoice, CheeseChoice, PineChoice, PepperChoice, HamChoice);
+        }catch (IllegalPizza illegalPizza) {
+            JOptionPane.showMessageDialog(null, "Invalid Pizza Configuration!" ,"Oh, no!",  JOptionPane.PLAIN_MESSAGE);
+        }
+        double UPrice = pizza.getCost();
+        UnitPrice.setText("CA$ " + UPrice);
+    }
 
 }
