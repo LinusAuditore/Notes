1b7a78d9a8a607adec7367515d6425dba27c4568
Xinghao Zuo <55333469+LinusAuditore@users.noreply.github.com>
2020-04-25 13:32:55 +0800

    Update Project: 营销号Generator

--- Java_Note/个人项目/ArticleGenerator/src/sample/Main.java
+++ Java_Note/个人项目/ArticleGenerator/src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("营销号文案生成器");
+        primaryStage.setTitle("营销号文案生成器 (Ver. 114514, Build. 1919810)");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
